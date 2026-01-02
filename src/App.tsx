da7c50dc9e9de2548fca4edea29a1745553b4c3b
saurabh-sayin <saurabh@sayinprojects.in>
2026-01-02 12:40:36 +0530

    minor chnage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-      <p> this is website for sony and deepak </p>
+      <p> this is website for Supriya and Deepak Wedding </p>
       </div>
     </>
   )
